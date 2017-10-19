c20f6453029184bd36a98ab4cc67cd454dc1bf8a
Jon Bulmer <jon.bulmer@gmail.com>
2017-10-19 18:09:51 +0100

    Added a test file for the company detail
    A test for the company detail component. This is the start of a set of tests so I can move forward with Test Driven Development going forward.

---
+++ src/app/companies/company-detail.component.spec.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'company-detail-app'
+	templateUrl: './company-detail.component.html',
+	styleUrls: ['./company-detail.component.css']
+})
+export class CompanyDetailComponent { }
