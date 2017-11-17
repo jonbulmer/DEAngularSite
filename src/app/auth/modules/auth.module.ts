5b85425e23f10ce5a98e3da369a857ace07c8f36
Jon Bulmer <jon.bulmer@gmail.com>
2017-11-17 22:13:01 +0000

    Open ID Connect work

--- src/app/auth/modules/auth.module.ts
+++ src/app/auth/modules/auth.module.ts
@@ -1,1 +1,29 @@
-	auto_clean_state_after_authentication: true;
+import { NgModule, ModuleWithProviders } from '@angular/core';
+
+import { OidcSecurityService } from '../services/oidc.security.service';
+import { AuthConfiguration, DefaultConfiguration } from './auth.configuration';
+import { NgModel } from '@angular/forms/src/directives/ng_model';
+
+@NgModule()
+export class AuthModule {
+    static forRoot(token: Token = {}): ModuleWithProviders {
+        return {
+            ngModule: AuthModule,
+            providers: [
+                OidcSecurityService
+            ]
+        }
+    }
+}
+
+export interface Type<T> extends Function {
+    
+        new (...args: any[]): T;
+    
+    }
+    
+    export interface Token {
+    
+        storage?: Type<any>;
+    
+    }
