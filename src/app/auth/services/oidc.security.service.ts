1877505a7101be08fc33e008cca1a96b7b782155
Jon Bulmer <jon.bulmer@gmail.com>
2017-11-19 10:39:08 +0000

    Adding more parts to the oidc setup

--- src/app/auth/services/oidc.security.service.ts
+++ src/app/auth/services/oidc.security.service.ts
@@ -1,5 +1,22 @@
-import { Injectable } from "@angular/core";
+import { Injectable, EventEmitter, Output } from "@angular/core";
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/timeInterval';
+import 'rxjs/add/operator/pluck';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/observable/timer';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Router } from '@angular/router';
+import { AuthConfiguration, OpenIDImplicitFlowConfiguration  } from '../modules/auth.configuration';
 
+import { JwtKeys } from './jwtkeys';
+import { AuthorizationResult } from './authorization-result.enum';
+import { UriEncoder } from './uri-encoder';
 
+/**
+ * Implement this class-interface to create a custom storage.
+ */
 @Injectable()
 export class OidcSecurityService {}
