07f38e962115d1048a78c06eebfa475e4f16456c
Jon Bulmer <jon.bulmer@gmail.com>
2017-12-04 08:46:25 +0000

    Continue to build the DataEventsRecord service

--- src/app/admin/dataeventrecords/dataeventrecords.service.ts
+++ src/app/admin/dataeventrecords/dataeventrecords.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { Configuration } from '../../app.constants';
+import { DataEventRecord } from './models/DataEventRecord';
 
 @Injectable()
 export class DataEventRecordsService {
+    private actionUrl: string;
+    private headers: HttpHeaders;
 
+    constructor(private http: HttpClient, _configuration: Configuration) {
+        this.actionUrl = `${_configuration.Server}api/DataEventRecords/`;
+    }
 }
