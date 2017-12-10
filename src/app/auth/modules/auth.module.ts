dd134fb2eb8a443021c377c73495639b58be7096
Jon Bulmer <jon.bulmer@gmail.com>
2017-12-10 21:22:52 +0000

    More security updates

--- src/app/auth/modules/auth.module.ts
+++ src/app/auth/modules/auth.module.ts
@@ -2,7 +2,16 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 
 import { OidcSecurityService } from '../services/oidc.security.service';
 import { AuthConfiguration, DefaultConfiguration } from './auth.configuration';
-import { NgModel } from '@angular/forms/src/directives/ng_model';
+import { OidcSecurityValidation } from '../services/oidc.security.validation';
+import { OidcSecurityCheckSession } from '../services/oidc.security.check-session';
+import { OidcSecuritySilentRenew } from '../services/oidc.security.silent-renew';
+import { OidcSecurityUserService } from '../services/oidc.security.user-service';
+import { OidcSecurityCommon } from '../services/oidc.security.common';
+import {
+    OidcSecurityStorage,
+    BrowserStorage
+} from '../services/oidc.security.storage';
+import { AuthWellKnownEndpoints } from '../services/auth.well-known-endpoints';
 
 @NgModule()
 export class AuthModule {
@@ -10,20 +19,28 @@ export class AuthModule {
         return {
             ngModule: AuthModule,
             providers: [
-                OidcSecurityService
+                OidcSecurityService,
+                OidcSecurityValidation,
+                OidcSecurityCheckSession,
+                OidcSecuritySilentRenew,
+                OidcSecurityUserService,
+                OidcSecurityCommon,
+                AuthConfiguration,
+                DefaultConfiguration,
+                AuthWellKnownEndpoints,
+                {
+                    provide: OidcSecurityStorage,
+                    useClass: token.storage || BrowserStorage
+                }
             ]
-        }
+        };
     }
 }
 
-export interface Type<T> extends Function {
-    
+export interface Type<T> extends Function { 
         new (...args: any[]): T;
-    
     }
     
     export interface Token {
-    
         storage?: Type<any>;
-    
     }
