92e1078658799f2fea5ad0605012118f4507afdc
Jon Bulmer <jon.bulmer@gmail.com>
2018-01-03 20:12:02 +0000

    Updates the security validation

--- src/app/auth/services/oidc.security.validation.ts
+++ src/app/auth/services/oidc.security.validation.ts
@@ -1,60 +1,77 @@
 import { Injectable } from '@angular/core';
-import { OidcSecurityCommon } from './oidc.security.common';
 
 import { KJUR, KEYUTIL, hextob64u } from 'jsrsasign';
+import { ArrayHelperService } from './oidc-array-helper.service';
+import { TokenHelperService } from './oidc-token-helper.service';
+import { LoggerService } from './oidc.logger.service';
 
 // http://openid.net/specs/openid-connect-implicit-1_0.html
 
 // id_token
 // id_token C1: The Issuer Identifier for the OpenID Provider (which is typically obtained during Discovery) 
 //   MUST exactly match the value of the iss (issuer) Claim.
+//
 // id_token C2: The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the 
 //   Issuer identified by the iss (issuer) Claim as an audience.The ID Token MUST be rejected if the ID Token does not 
 //   list the Client as a valid audience, or if it contains additional audiences not trusted by the Client.
+//
 // id_token C3: If the ID Token contains multiple audiences, the Client SHOULD verify that an azp Claim is present.
+//
 // id_token C4: If an azp (authorized party) Claim is present, the Client SHOULD verify that its client_id is the Claim Value.
+//
 // id_token C5: The Client MUST validate the signature of the ID Token according to JWS [JWS] using the algorithm specified 
 //   in the alg Header Parameter of the JOSE Header. The Client MUST use the keys provided by the Issuer.
+//
 // id_token C6: The alg value SHOULD be RS256. Validation of tokens using other signing algorithms is described in 
 //   the OpenID Connect Core 1.0 [OpenID.Core] specification.
+//
 // id_token C7: The current time MUST be before the time represented by the exp Claim (possibly allowing for some 
 //   small leeway to account for clock skew).
+//
 // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time, limiting 
 //   the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
+//
 // id_token C9: The value of the nonce Claim MUST be checked to verify that it is the same value as the one that was 
 //   sent in the Authentication Request.The Client SHOULD check the nonce value for replay attacks.The precise method for 
 //   detecting replay attacks is Client specific.
+//
 // id_token C10: If the acr Claim was requested, the Client SHOULD check that the asserted Claim Value is appropriate.
 //   The meaning and processing of acr Claim Values is out of scope for this document.
+//
 // id_token C11: When a max_age request is made, the Client SHOULD check the auth_time Claim value and request 
 //   re- authentication if it determines too much time has elapsed since the last End- User authentication.
-
-//// Access Token Validation
+// Access Token Validation
 // access_token C1: Hash the octets of the ASCII representation of the access_token with the hash algorithm specified 
 // in JWA[JWA] for the alg Header Parameter of the ID Token's JOSE Header. For instance, if the alg is RS256, 
 // the hash algorithm used is SHA-256.
 // access_token C2: Take the left- most half of the hash and base64url- encode it.
 // access_token C3: The value of at_hash in the ID Token MUST match the value produced in the previous step if 
 // at_hash is present in the ID Token.
-
 @Injectable()
 export class OidcSecurityValidation {
-    constructor(private oidcSecurityCommon: OidcSecurityCommon) {
+    constructor(
+        private arrayHelperService: ArrayHelperService,
+        private tokenHelperService: TokenHelperService,
+        private loggerService: LoggerService) {
     }
 
     // id_token C7: The current time MUST be before the time represented by the exp Claim 
     // (possibly allowing for some small leeway to account for clock skew).
     isTokenExpired(token: string, offsetSeconds?: number): boolean {
-        
         let decoded: any;
-        decoded = this.getPayloadFromToken(token, false);
+        decoded = this.tokenHelperService.getPayloadFromToken(token, false);
         return !(this.validate_id_token_exp_not_expired(decoded, offsetSeconds));        
     }
         
     // id_token C7: The current time MUST be before the time represented by the exp Claim 
     //   (possibly allowing for some small leeway to account for clock skew).
-    validate_id_token_exp_not_expired(decoded_id_token: string, offsetSeconds?: number): boolean {
-        let tokenExpirationDate = this.getTokenExpirationDate(decoded_id_token);
+    validate_id_token_exp_not_expired(
+        decoded_id_token:
+        string, offsetSeconds?: number
+    ): boolean {
+        const tokenExpirationDate = this.tokenHelperService.getTokenExpirationDate(
+            decoded_id_token
+        );
         offsetSeconds = offsetSeconds || 0;
         
         if (tokenExpirationDate == null) {
@@ -62,7 +79,9 @@ export class OidcSecurityValidation {
         }
                 
         // Token not expired?
-        return (tokenExpirationDate.valueOf() > (new Date().valueOf() + (offsetSeconds * 1000)));       
+        return (
+            tokenExpirationDate.valueOf() >
+            new Date().valueOf() + (offsetSeconds * 1000       
     }
 
 
@@ -96,31 +115,40 @@ export class OidcSecurityValidation {
 // 1970- 01 - 01T00: 00:00Z as measured in UTC until the date/ time.
 
 validate_required_id_token(dataIdToken: any): boolean {
-            
     let validated = true;
     if (!dataIdToken.hasOwnProperty('iss')) {
         validated = false;   
-        this.oidcSecurityCommon.logWarning('iss is missing, this is required in the id_token');        
+        this.loggerService.logWarning(
+            'iss is missing, this is required in the id_token'
+        );        
     }
         
     if (!dataIdToken.hasOwnProperty('sub')) {
         validated = false;
-        this.oidcSecurityCommon.logWarning('sub is missing, this is required in the id_token');
+        this.loggerService.logWarning(
+            'sub is missing, this is required in the id_token'
+        );
     }
         
     if (!dataIdToken.hasOwnProperty('aud')) {
         validated = false;
-        this.oidcSecurityCommon.logWarning('aud is missing, this is required in the id_token');
+        this.loggerService.logWarning(
+            'aud is missing, this is required in the id_token'
+        );
     }
         
     if (!dataIdToken.hasOwnProperty('exp')) {
         validated = false;
-        this.oidcSecurityCommon.logWarning('exp is missing, this is required in the id_token');
+        this.loggerService.logWarning(
+            'exp is missing, this is required in the id_token'
+        );
     }
         
     if (!dataIdToken.hasOwnProperty('iat')) {
         validated = false;
-        this.oidcSecurityCommon.logWarning('iat is missing, this is required in the id_token');
+        this.loggerService.logWarning(
+            'iat is missing, this is required in the id_token'
+        );
     }
         
     return validated;    
@@ -128,7 +156,10 @@ validate_required_id_token(dataIdToken: any): boolean {
 
 // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
 //   limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
-validate_id_token_iat_max_offset(dataIdToken: any, max_offset_allowed_in_seconds: number): boolean {
+validate_id_token_iat_max_offset(
+    dataIdToken: any,
+    max_offset_allowed_in_seconds: number
+): boolean {
     if (!dataIdToken.hasOwnProperty('iat')) {
         return false;
     }
@@ -142,8 +173,14 @@ validate_id_token_iat_max_offset(dataIdToken: any, max_offset_allowed_in_seconds
         return false;
     }
 
-    this.oidcSecurityCommon.logDebug('validate_id_token_iat_max_offset: ' + (new Date().valueOf() - dateTime_iat_id_token.valueOf()) + ' < ' + (max_offset_allowed_in_seconds * 1000));
-    return ((new Date().valueOf() - dateTime_iat_id_token.valueOf()) < (max_offset_allowed_in_seconds * 1000));
+    this.loggerService.logDebug(
+        'validate_id_token_iat_max_offset: ' + 
+        (new Date().valueOf() - dateTime_iat_id_token.valueOf()) +
+        ' < ' + 
+        max_offset_allowed_in_seconds * 1000);
+        return (
+            new Date().valueOf() - dateTime_iat_id_token.valueOf() <
+            max_offset_allowed_in_seconds * 1000);
 }
 
 // id_token C9: The value of the nonce Claim MUST be checked to verify that it is the same value as the one that was 
@@ -151,7 +188,12 @@ validate_id_token_iat_max_offset(dataIdToken: any, max_offset_allowed_in_seconds
 //   detecting replay attacks is Client specific.
 validate_id_token_nonce(dataIdToken: any, local_nonce: any): boolean {
     if (dataIdToken.nonce !== local_nonce) {
-        this.oidcSecurityCommon.logDebug('Validate_id_token_nonce failed, dataIdToken.nonce: ' + dataIdToken.nonce + ' local_nonce:' + local_nonce);
+        this.loggerService.logDebug(
+            'Validate_id_token_nonce failed, dataIdToken.nonce: ' + 
+            dataIdToken.nonce + 
+            ' local_nonce:' + 
+            local_nonce
+        );
         return false;
     }
 
@@ -161,9 +203,19 @@ validate_id_token_nonce(dataIdToken: any, local_nonce: any): boolean {
 
 // id_token C1: The Issuer Identifier for the OpenID Provider (which is typically obtained during Discovery) MUST 
 // exactly match the value of the iss (issuer) Claim.
-validate_id_token_iss(dataIdToken: any, authWellKnownEndpoints_issuer: any): boolean {
-    if (dataIdToken.iss != authWellKnownEndpoints_issuer) {
-        this.oidcSecurityCommon.logDebug('Validate_id_token_iss failed, dataIdToken.iss: ' + dataIdToken.iss + ' authWellKnownEndpoints issuer:' + authWellKnownEndpoints_issuer);
+validate_id_token_iss(
+    dataIdToken: any, 
+    authWellKnownEndpoints_issuer: any
+): boolean {
+    if (
+        (dataIdToken.iss as string) !== 
+        (authWellKnownEndpoints_issuer as string)
+     ) {
+        this.loggerService.logDebug(
+            'Validate_id_token_iss failed, dataIdToken.iss: ' +
+            dataIdToken.iss + 
+            ' authWellKnownEndpoints issuer:' + 
+            authWellKnownEndpoints_issuer);
         return false;
     }
 
@@ -175,12 +227,35 @@ validate_id_token_iss(dataIdToken: any, authWellKnownEndpoints_issuer: any): boo
 // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience, or if it contains 
 // additional audiences not trusted by the Client.
 validate_id_token_aud(dataIdToken: any, aud: any): boolean {
-    if (dataIdToken.aud != aud) {
-        this.oidcSecurityCommon.logDebug('Validate_id_token_aud failed, dataIdToken.aud: ' + dataIdToken.aud + ' client_id:' + aud);
+    if (dataIdToken.aud instanceof Array) {
+        const result = this.arrayHelperService.arraysEqual(
+            dataIdToken.aud, 
+            aud
+        );
+        
+        if(!result) {
+            this.loggerService.logDebug(
+                'Validate_id_token_aud failed, dataIdToken.aud: ' + 
+                dataIdToken.aud + 
+                ' client_id:' + 
+                aud
+            );
+            return false;
+        }
+
+        return true;
+    } else if (dataIdToken.aud !== aud) {
+        this.loggerService.logDebug(
+            'Validate_id_token_aud failed, dataIdToken.aud: ' +
+            dataIdToken.aud +
+            ' client_id:' +
+            aud   
+        );
+
         return false;
     }
-
-    return true;
+     
+    return true;    
 }
 
 validateStateFromHashCallback(state: any, local_state: any): boolean {
