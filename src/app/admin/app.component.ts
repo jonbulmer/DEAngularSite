e50bf73696f4ba2c55badde5627e889e0fc84c83
Jon Bulmer <jon.bulmer@gmail.com>
2018-01-14 07:56:37 +0000

    fixed the app.component.ts file

--- src/app/admin/app.component.ts
+++ src/app/admin/app.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { OidcSecurityService } from '../auth/services/oidc.security.service';
-import './app.component.css';
 import { AuthorizationResult } from '../auth/models/authorization-result.enum';
 
 @Component({
     selector: 'app-component',
     templateUrl: 'app.component.html',
+    styleUrls: ['./app.component.css']
 })
 
 
@@ -30,67 +30,62 @@ export class AppComponent implements OnInit, OnDestroy {
                 this.doCallbackLogicIfRequired();
             });
         }        
-    }
 
-    this.oidcSecurityService.onCheckSessionChanged.subscribe(
+        this.oidcSecurityService.onCheckSessionChanged.subscribe(
         (checksession: boolean) => {
             console.log('...recieved a check session event');
             this.checksession = checksession;
         });
 
-    this.oidcSecurityService.onAuthorizationResult.subscribe(
+        this.oidcSecurityService.onAuthorizationResult.subscribe(
         (authorizationResult: AuthorizationResult) => {
             this.onAuthorizationResultComplete(authorizationResult);
         });
+    }
 
-        ngOnDestroy(): void {
-            this.isAuthorizedSubscription.unsubscribe();
-            this.oidcSecurityService.onModuleSetup.unsubscribe();
-            this.oidcSecurityService.onCheckSessionChanged.unsubscribe();
-            this.oidcSecurityService.onAuthorizationResult.unsubscribe();
+    ngOnInit() {
+        this.isAuthorizedSubscription = this.oidcSecurityService.getIsAuthorized().subscribe(
+            (isAuthorized: boolean) => {
+                this.isAuthorized = isAuthorized;
+            });
         }
 
-        ngOnInit() {
-            this.isAuthorizedSubscription = this.oidcSecurityService.getIsAuthorized().subscribe(
-                (isAuthorized: boolean) => {
-                    this.isAuthorized = isAuthorized;
-                });
-        }
+    ngOnDestroy(): void {
+        this.isAuthorizedSubscription.unsubscribe();
+        this.oidcSecurityService.onModuleSetup.unsubscribe();
+        this.oidcSecurityService.onCheckSessionChanged.unsubscribe();
+        this.oidcSecurityService.onAuthorizationResult.unsubscribe();
+    } 
 
-     
+    login() {
+        this.oidcSecurityService.authorize();
+    }
     
- 
-
-        login() {
-            this.oidcSecurityService.authorize();
-        }
+    refreshSession() {
+        console.log('start refreshSession');
+        this.oidcSecurityService.authorize();
+    }
     
-        refreshSession() {
-            console.log('start refreshSession');
-            this.oidcSecurityService.authorize();
-        }
-    
-        logout() {
-            console.log('start logoff');
-            this.oidcSecurityService.logoff();
-        }        
+    logout() {
+        console.log('start logoff');
+        this.oidcSecurityService.logoff();
+    }        
 
-        private doCallbackLogicIfRequired() {
-            if (window.location.hash) {
-                this.oidcSecurityService.authorizedCallback();
-            }
+    private doCallbackLogicIfRequired() {
+        if (window.location.hash) {
+            this.oidcSecurityService.authorizedCallback();
         }
+    }
 
-        private onAuthorizationResultComplete(authorizationResult: AuthorizationResult) {
-            console.log('Auth result received:' + authorizationResult);
-            if (authorizationResult === AuthorizationResult.unauthorized) {
-                if (window.parent) {
-                    // sent from the child iframe, for example the silent renew
-                    window.parent.location.href = '/unauthorized';
-                } else {
-                    // sent from the main window
-                    window.location.href = '/unauthorized';
-                }
+    private onAuthorizationResultComplete(authorizationResult: AuthorizationResult) {
+        console.log('Auth result received:' + authorizationResult);
+        if (authorizationResult === AuthorizationResult.unauthorized) {
+            if (window.parent) {
+                // sent from the child iframe, for example the silent renew
+                window.parent.location.href = '/unauthorized';
+            } else {
+                // sent from the main window
+                window.location.href = '/unauthorized';
             }
         }
     }
