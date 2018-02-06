ba9307922266c4e71ce98da87f3af5f759efa1a3
Jon Bulmer <jon.bulmer@gmail.com>
2018-02-06 09:02:37 +0000

    Fixes to the company service

--- src/app/companies/company.service.ts
+++ src/app/companies/company.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/throw';
+
 import { ICompany } from './company';
-import { HttpErrorResponse } from '@angular/common/http/src/response';
 
 @Injectable() 
 export class CompanyService {
     //private _companyUrl = 'src/api/company/company.json';
     private _companyUrl = 'https://diamondedge-api.co.uk/api/companies';
-    constructor(private _httpClient: HttpClient) { }
+    constructor(private _http: HttpClient) { }
 
     getCompanies(): Observable<ICompany[]> {
-        return this._httpClient.get<ICompany[]>(this._companyUrl, options);  
+        return this._http.get<ICompany[]>(this._companyUrl)
+        .do(data => console.log('All: ' + JSON.stringify(data)))
+        .catch(this.handleError);
     };
 
     getCompany(id: number): Observable<ICompany> {
@@ -23,8 +25,14 @@ export class CompanyService {
              .map((companies: ICompany[]) => companies.find(c => c.Id === id));
     }
 
-    private handleError(error: HttpErrorResponse) {
-       console.error(error); 
-       return Observable.throw(json().error || 'Server error')
+    private handleError(err: HttpErrorResponse) {
+        let errorMessage = '';
+        if (err.error instanceof Error) {
+            errorMessage = `An error occurred: ${err.error.message}`;
+        } else {
+            errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.error(errorMessage);
+        return Observable.throw(errorMessage);
     }
 }
